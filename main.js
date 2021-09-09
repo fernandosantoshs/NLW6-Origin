10620db8e0826e184145e881cf52ed55d69ba5e3
fernandosantoshs <44985596+fernandosantoshs@users.noreply.github.com>
2021-09-09 20:57:36 -0300

    Feature: Style improvement, Swiper and ScrollJS

--- main.js
+++ main.js
@@ -29,3 +29,31 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll')
     }
 })
+
+/* Testimonials Swiper */
+
+const swiper = new Swiper('.swiper-container', {
+    slidesperview: 1,
+    pagination: {
+        el:'.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+})
+
+/* Scroll Reveal : Show elements while scrolling */
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700, 
+    reset: true
+})
+
+scrollReveal.reveal(`
+#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links`, 
+{ interval: 100 })
