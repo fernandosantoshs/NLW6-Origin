f0d50fed76c525751d5451bed0c52b6c683e78bc
fernandosantoshs <44985596+fernandosantoshs@users.noreply.github.com>
2021-09-25 23:01:45 -0300

    Feature: construcao do footer e termino do style mobo
    Finalizado style para dispositivos mobo, construção do footer e do botao de voltar ao topo.

--- main.js
+++ main.js
@@ -1,59 +1,76 @@
 /* Toggle Menu */
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+const nav = document.querySelector("#header nav")
+const toggle = document.querySelectorAll("nav .toggle")
 
 for (const element of toggle) {
-    element.addEventListener('click', function() {
-        nav.classList.toggle('show')
-    })
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show")
+  })
 }
 
 /* Selecao dos itens do menu */
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll("nav ul li a");
 
 for (link of links) {
-    link.addEventListener('click', function() {
-        nav.classList.remove('show')
-    })
+  link.addEventListener("click", function () {
+    nav.classList.remove("show")
+  })
 }
 
 /* Header scroll */
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function() {
-    if (window.scrollY >= navHeight) {
-        header.classList.add('scroll')
-    } else {
-        header.classList.remove('scroll')
-    }
-})
+function changeHeaderOnScroll() {
+  const header = document.querySelector("#header")
+  const navHeight = header.offsetHeight
+  if (window.scrollY >= navHeight) {
+    header.classList.add("scroll")
+  } else {
+    header.classList.remove("scroll")
+  }
+}
 
 /* Testimonials Swiper */
 
-const swiper = new Swiper('.swiper-container', {
-    slidesperview: 1,
-    pagination: {
-        el:'.swiper-pagination'
-    },
-    mousewheel: true,
-    keyboard: true
+const swiper = new Swiper(".swiper-container", {
+  slidesperview: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
 })
 
 /* Scroll Reveal : Show elements while scrolling */
 
 const scrollReveal = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 700, 
-    reset: true
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
 })
 
-scrollReveal.reveal(`
-#home .image, #home .text,
+scrollReveal.reveal(
+`#home .image, #home .text,
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links`, 
-{ interval: 100 })
+#contact .text, #contact .links,
+footer .brand, footer .social`,
+  { interval: 100 }
+)
+
+// Back to top button
+function backToTop(){
+    const backToTopButton = document.querySelector(".back-to-top");
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add("show")
+    } else {
+        backToTopButton.classList.remove("show")
+    }
+}
+
+// Page Scroll 
+window.addEventListener("scroll", function(){
+    changeHeaderOnScroll()
+    backToTop()
+})
